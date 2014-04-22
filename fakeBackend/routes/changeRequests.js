8ed35b7cae42caa4879cc9ee8198252a516e200b
ntquyen <nguyen.tuan.quyen.it@gmail.com>
2014-04-22 11:15:51 +0700

    Delete fakebackend routes

--- fakeBackend/routes/changeRequests.js
+++
@@ -1,6 +0,0 @@
-exports.getChangeRequests = function(req, res, next) {
-    res.send(200, []);
-};
-exports.create = function(req, res, next) {
-    res.send(201, {});
-}
--- fakeBackend/routes/fileUploads.js
+++
@@ -1,6 +0,0 @@
-exports.verify = function(req, res, next) {
-    res.send(200, {});
-};
-exports.import = function(req, res, next) {
-    res.send(200, {});
-};
